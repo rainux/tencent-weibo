f7dddc8fbc2cd0f561efe25b42a14b7b59d4dcee
Rainux Luo <rainux@gmail.com>
2013-06-16 04:01:08 +0800

    Correctly handle invalid UTF-8 characters in Tencent's JSON response

--- lib/tencent/weibo/response.rb
+++ lib/tencent/weibo/response.rb
@@ -1,13 +1,13 @@
 OAuth2::Response.register_parser(:tencent_json, []) do |body|
   begin
-    body.force_encoding(Encoding::UTF_8)
-    MultiJson.load(body)
-  rescue MultiJson::LoadError => e
-    if e.message.include?('lexical error: invalid bytes in UTF8 string.')
-      valid_body = body.encode(Encoding::UTF_16BE, invalid: :replace).encode(Encoding::UTF_8)
-      MultiJson.load(valid_body)
-    else
-      raise
+    body_as_utf8 = body.force_encoding(Encoding::UTF_8)
+
+    unless body_as_utf8.valid_encoding?
+      body_as_utf8 = body.
+        encode(Encoding::UTF_16BE, invalid: :replace).
+        encode(Encoding::UTF_8)
     end
+
+    MultiJson.load(body_as_utf8)
   end
 end
